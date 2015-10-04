70b7fc54ff9f2b6708a8dc393c6e0ce14258c481
Bill Crum <bill@ezm2m.com>
2015-10-04 10:03:08 -0500

    add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Do yard',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to store',
-	completed: false
-}, {
-	id: 3,
-	description: 'train',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('todo API Root');
@@ -38,6 +30,15 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	}
 });
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	res.json(body);
+})
 
 app.get('/todos')
 app.listen(PORT, function() {
